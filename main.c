1c887c635bf6e5e9708091db799c8e6780099439
yo-irne <margody5@gmail.com>
2023-01-08 14:25:54 +0100

    Jakiśtam początek

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int ekalg(int** C, int** E, int n, int s, int t) {
+	int f = 0;
+	int F[n][n];
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			F[i][j] = 0;
+		}
+	}
+	cont = 1;
+	while(cont) {
+
+	}
+}
+
+int main() {
+
+	return 0;
+}
